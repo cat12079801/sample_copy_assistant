9e1e220399e914028f4b2b635de39880b91905f4
cat12079801 <cat12079801@gmail.com>
2016-01-13 16:20:47 +0900

    コピー成功/失敗時のアニメーションを追加

--- js/content_scripts/common.js
+++ js/content_scripts/common.js
@@ -1,13 +1,26 @@
 $(function(){
   $('div.io-style').nextAll().filter('div.part:even').each(function(i){
-    $(this).find('pre').attr('id', 'input-sample-'+(i+1)).after('<button class="btn btn-primary btn-large" data-clipboard-target="#input-sample-' + (i+1) + '">Copy</button>');
+    $(this).find('pre')
+      .attr('id', 'input-sample-'+(i+1))
+      .addClass('input-sample')
+      .after('<button class="btn btn-primary btn-large" data-clipboard-target="#input-sample-' + (i+1) + '">Copy</button>');
   });
 });
 
 var clipboard = new Clipboard('.btn');
 clipboard.on('success', function(e) {
-//成功時の処理
+  pre_tag = e["trigger"]["dataset"]["clipboardTarget"];
+  if(!$(pre_tag).hasClass('success_animating')){
+    $(pre_tag).addClass('success_animating').delay(1000).queue(function(){
+      $(this).removeClass('success_animating').dequeue();
+    });
+  }
 });
 clipboard.on('error', function(e) {
-//失敗時の処理
+  pre_tag = e["trigger"]["dataset"]["clipboardTarget"];
+  if(!$(pre_tag).hasClass('failed_animating')){
+    $(pre_tag).addClass('failed_animating').delay(1000).queue(function(){
+      $(this).removeClass('failed_animating').dequeue();
+    });
+  }
 });
